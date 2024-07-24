93e08e27b0954826dc4ad22d64d789633c2522b4
Bikram Mondal <bikramm8617@gmail.com>
2024-07-24 12:05:23 +0530

    added loader component for implenting lazy loading and resize the images for better loading speed

--- frontend/src/components/pages/Home.component.js
+++ frontend/src/components/pages/Home.component.js
@@ -1,30 +1,27 @@
-import { lazy, Suspense } from "react";
-import { useChangeTitle } from "../../utills";
-import { CARD_DATA } from "../../data/data";
-import Slider from "../slider/Slider.component";
-import TextSection from "../text-section/TextSection.component";
-import Card from "../card/Card.component";
-const Feature = lazy(() => import("../feature/Feature.component"));
+import { lazy, Suspense } from 'react';
+import { useChangeTitle } from '../../utills';
+import { CARD_DATA } from '../../data/data';
+import Slider from '../slider/Slider.component';
+import Card from '../card/Card.component';
+import Loading from '../loader/Loading.component';
+const TextSection = lazy(() => import('../text-section/TextSection.component'));
+const Feature = lazy(() => import('../feature/Feature.component'));
 
 const Home = () => {
-  useChangeTitle("");
+  useChangeTitle('');
   return (
     <>
       <Slider />
-      <div className='padding min-h-screen bg-gray-800 text-white'>
-        <article className='pb-8 grid md:grid-cols-2 lg:grid-cols-4 gap-6'>
+      <div className="padding min-h-screen bg-gray-800 text-white">
+        <article className="pb-8 grid md:grid-cols-2 lg:grid-cols-4 gap-6">
           {CARD_DATA.map((item, i) => (
             <Card key={i} data={item} />
           ))}
         </article>
-        <Suspense
-          fallback={
-            <h2 className='text-center text-xl text-white'>Loading...</h2>
-          }
-        >
+        <Suspense fallback={<Loading />}>
           <Feature />
+          <TextSection />
         </Suspense>
-        <TextSection />
       </div>
     </>
   );
