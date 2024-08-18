e6a6fc02238c359e726eae9a14f644088b2f2a08
Bikram Mondal <bikramm8617@gmail.com>
2024-08-18 16:10:34 +0530

    small change in blog item component

--- frontend/src/components/comment/Comment.component.js
+++ frontend/src/components/comment/Comment.component.js
@@ -31,7 +31,7 @@ const Comment = ({ blogId }) => {
   };
   return (
     <div className="bg-gray-900 text-white">
-      <div className="p-8 sm:p-16 md:p-28 flex flex-col gap-6 ">
+      <div className="p-6 sm:p-16 md:p-28 flex flex-col gap-6 ">
         <h2 className="text-3xl font-semibold">Leave a Reply</h2>
         <p className="font-semibold">
           Your email address will not be published. Required fields are marked *
